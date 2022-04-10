1c8315dc383f3e60ed735e5d49ebc0a98638a524
Nim09911 <nim09911@NIMIT-JAINS-LAPTOP.localdomain>
2022-04-10 20:54:08 +0530

    chore: chatkit discontinued find alternative

--- src/pages/CommunityChat/components/MessageList.js
+++ src/pages/CommunityChat/components/MessageList.js
@@ -2,35 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Message from './Message'
 
+
 class MessageList extends React.Component {
-    componenetWilUpdate() {
-        const node = ReactDOM.findDOMNode(this);
-        this.shouldScrollBottom = node.scrollTop + node.clientHeight + 50 >= node.scrollHeight;
-    }
-
-    componentDidUpdate() {
-        if(this.shouldScrollBottom) {
-            const node = ReactDOM.findDOMNode(this);
-            node.scrollTop = node.scrollHeight
-        }
-    }
-
     render() {
-        if(!this.props.currRoomId) {
-            <div className="message-list">
-                <div className="join-room">
-                    &larr; Join a room!
-                </div>
-            </div>
-        }
-        return(
-            <div className="message-list">
+        return (
+            <ul className="message-list">
                 {this.props.messages.map((message, index) => {
-                    return <Message key={index} user={message.senderId} text={message.text} />
+                    return (
+                      <li  key={message.id} className="message">
+                        <div>{message.senderId}</div>
+                        <div>{message.text}</div>
+                      </li>
+                    )
                 })}
-            </div>
+            </ul>
         )
     }
 }
-
 export default MessageList
--- src/pages/CommunityChat/components/SendMessageForm.js
+++ src/pages/CommunityChat/components/SendMessageForm.js
@@ -1,41 +1,41 @@
 import React from 'react'
-
 class SendMessageForm extends React.Component {
-    
-    constructor() {
+  constructor() {
       super()
       this.state = {
-        message: ''
+          message: ''
       }
       this.handleChange = this.handleChange.bind(this)
       this.handleSubmit = this.handleSubmit.bind(this)
-    }
-
-    handleChange(e) {
+  }
+  
+  handleChange(e) {
       this.setState({
-        message: e.target.value
+          message: e.target.value
       })
-    }
-
-    handleSubmit(e) {
+  }
+  
+  handleSubmit(e) {
       e.preventDefault()
       this.props.sendMessage(this.state.message)
       this.setState({
-        message: ''
+          message: ''
       })
-    }
-
-    render() {
-      return (
-        <form
-          onSubmit={this.handleSubmit}
-          className="send-message-form">
-          <input
-            onChange={this.handleChange}
-            value={this.state.message}
-            placeholder="Type your message and hit ENTER"
-            type="text" />
-        </form>
-      )
-    }
   }
+  
+  render() {
+      return (
+          <form
+              onSubmit={this.handleSubmit}
+              className="send-message-form">
+              <input
+                  onChange={this.handleChange}
+                  value={this.state.message}
+                  placeholder="Type your message and hit ENTER"
+                  type="text" />
+          </form>
+      )
+  }
+}
+
+export default SendMessageForm
